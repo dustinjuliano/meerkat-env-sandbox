04dd1deea0309ef35d17bd96648f1da98fd428d8
Dustin Juliano <djuliano@andrew.cmu.edu>
2026-07-05 14:15:50 -0400

    refactor: add sibling blocks to sample program

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 /*
 Sample Program Structure:
-{
+{ // Block 1
   let a = "a";
   let b = "b1";
-  {
+  { // Block 2
     let b = "b2";
-    {
-      let c = "c";
+    { // Block 3
+      let c = "c1";
     }
+    { // Block 4
+      let b = "b3";
+      let c = "c2";
+    }
+    let c = "c3";
   }
   let d = "d";
 }
@@ -19,7 +24,7 @@ Symbol Association:
 - "d" => Symbol(4)
 */
 
-use env::env::{Context, EntryId, RegionId, Symbol};
+use env::env::{Context, EntryId, RegionId, Symbol, Iter};
 
 #[allow(dead_code)]
 #[derive(Debug)]
@@ -28,25 +33,26 @@ struct Entry {
   ty: String,
 }
 
-#[allow(dead_code)]
 struct Program {
   ctx: Context,
   mem: Vec<Entry>,
   root_region: RegionId,
 }
 
-// Simulates static lexical scoping
-fn build_program() -> Option<Program> {
+fn simulate_lexical_scoping() -> Option<Program> {
   let mem = vec![
     Entry { value: "a".to_string(), ty: "String".to_string() },
     Entry { value: "b1".to_string(), ty: "String".to_string() },
     Entry { value: "b2".to_string(), ty: "String".to_string() },
-    Entry { value: "c".to_string(), ty: "String".to_string() },
+    Entry { value: "c1".to_string(), ty: "String".to_string() },
+    Entry { value: "b3".to_string(), ty: "String".to_string() },
+    Entry { value: "c2".to_string(), ty: "String".to_string() },
+    Entry { value: "c3".to_string(), ty: "String".to_string() },
     Entry { value: "d".to_string(), ty: "String".to_string() },
   ];
 
   let mut ctx = Context::new();
-  let root_region = ctx.region_alloc(5);
+  let root_region = ctx.region_alloc(8);
   let mut i = ctx.iter_mut(root_region)?;
 
   i.bind(Symbol(1), EntryId(0));
@@ -59,9 +65,15 @@ fn build_program() -> Option<Program> {
   i.bind(Symbol(3), EntryId(3));
 
   i = i.up()?;
-  i = i.up()?;
+  i.push();
+  i.bind(Symbol(2), EntryId(4));
+  i.bind(Symbol(3), EntryId(5));
 
-  i.bind(Symbol(4), EntryId(4));
+  i = i.up()?;
+  i.bind(Symbol(3), EntryId(6));
+
+  i = i.up()?;
+  i.bind(Symbol(4), EntryId(7));
 
   Some(Program {
     ctx,
@@ -70,33 +82,51 @@ fn build_program() -> Option<Program> {
   })
 }
 
-/// Simulates evaulator using pre-computed lexical scopes
-fn walk_and_lookup(p: &Program) -> Option<()> {
+fn find<'a>(p: &'a Program, cursor: Iter, sym: Symbol) -> Option<&'a Entry> {
+  let eid = cursor.find(sym)?;
+  Some(&p.mem[eid.0 as usize])
+}
+
+fn simulate_eval(p: &Program) -> Option<()> {
   let mut cursor = p.ctx.iter(p.root_region)?;
 
-  cursor.find(Symbol(2));
-  cursor.find(Symbol(3));
+  // Block 1
+  find(p, cursor, Symbol(1));
+  find(p, cursor, Symbol(2));
+  find(p, cursor, Symbol(3));
 
+  // Enter Block 2
   cursor = cursor.down()?;
+  find(p, cursor, Symbol(2));
+  find(p, cursor, Symbol(3));
 
-  cursor.find(Symbol(2));
-  cursor.find(Symbol(3));
-
+  // Enter Block 3
   cursor = cursor.down()?;
+  find(p, cursor, Symbol(3));
+  find(p, cursor, Symbol(2));
 
-  cursor.find(Symbol(3));
-  cursor.find(Symbol(2));
-  cursor.find(Symbol(1));
-  cursor.find(Symbol(4));
-
+  // Exit Block 3
   cursor = cursor.up()?;
-  let _ = cursor.up()?;
+
+  // Enter Block 4
+  cursor = cursor.down()?;
+  cursor.next()?;
+  find(p, cursor, Symbol(2));
+  find(p, cursor, Symbol(3));
+
+  // Exit Block 4 (returns to Block 2)
+  cursor = cursor.up()?;
+  find(p, cursor, Symbol(3));
+
+  // Exit Block 2 (returns to Block 1)
+  cursor = cursor.up()?;
+  find(p, cursor, Symbol(4));
 
   Some(())
 }
 
 fn main() -> Result<(), &'static str> {
-  let pg = build_program().ok_or("build error")?;
-  walk_and_lookup(&pg).ok_or("find error")?;
+  let pg = simulate_lexical_scoping().ok_or("build error")?;
+  simulate_eval(&pg).ok_or("find error")?;
   Ok(())
 }
